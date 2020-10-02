1a0cd52c8bd6b5aa90363a46d095b2799b874305
carespu <52917956+carespu@users.noreply.github.com>
2020-10-02 10:03:58 +0200

    Update gráfico de coordenadasSR.r

--- gráfico de coordenadasSR.r
+++ gráfico de coordenadasSR.r
@@ -1,3 +1,4 @@
+
 rm(list=ls())
 Sys.setenv(TZ='GMT')
 Sys.setlocale("LC_TIME", "English")
@@ -5,23 +6,24 @@ par(mfrow=c(1,1))
 library(ggplot2)
 library(akima)
 
-setwd('')#set working directory
+setwd('c:\..\YOURpath\') #set your working directory 
 
-par(mfrow=c(1,2))
 
-#Build coordinates graph
+#Build coordinates graph:
 
-plantas<-read.table('datos generales.todos.txt',header=T)#import dataframe, we have several repetitions in our data frame for each individual
-plot(Latitude~Longitude, col=especie, data=plantas) # show plants distribution
+plants<-read.table('yourdata.txt',header=T)# import dataframe, we have several in my case I imported a df with plant distribution in a field with 
+# temperature and soil respiration variables, and several dais of mesurements for all the individuals, I have repetitions in my data frame for each individual
 
-a<-aggregate( . ~Plot, plantas, mean )# build df with mean values for each individual from our dataset
+plot(Latitude~Longitude, col=specie, data=plants) # show plants distribution in a plot to know yor data, in my case I had diferent species
+
+a<-aggregate( . ~Plot, plants, mean )# I want to sea the mean values, so I build df with mean values for each individual from our dataset
 
 gpsli <- interp(a$Longitude, a$Latitude, a$temp, duplicate = "median")#use interp function from akima to create interpolation for soil temperature
 
-image (gpsli, main = "Soil Temperature", ylab = 'Longitude', xlab = 'Latitude')#draw 
-contour(gpsli, add=TRUE) 
+image (gpsli, main = "Soil Temperature", ylab = 'Longitude', xlab = 'Latitude')#draw the raster
+contour(gpsli, add=TRUE) #draw the contourn
 
-## increase smoothness (using finer grid):
+## we can increase increase smoothness (using finer grid), in this example I use soil respiration :
 gpssmooth <-  with(a, interp(Longitude, Latitude, SR, xo=seq(min(Longitude),max(Longitude), length=100), yo=seq(min(Latitude),max(Latitude), length=100), duplicate = "median"))
 image (gpssmooth, main = "Soil Respiration")
 contour(gpssmooth, add=T)
