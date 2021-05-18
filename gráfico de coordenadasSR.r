627dc923eee8088be2dd8b85ad3344627fe17cf3
carespu <52917956+carespu@users.noreply.github.com>
2021-05-18 15:29:21 +0200

    Update gráfico de coordenadasSR.r

--- gráfico de coordenadasSR.r
+++ gráfico de coordenadasSR.r
@@ -16,7 +16,7 @@ plants<-read.table('yourdata.txt',header=T)# import dataframe, we have several i
 
 plot(Latitude~Longitude, col=specie, data=plants) # show plants distribution in a plot to know yor data, in my case I had diferent species
 
-a<-aggregate( . ~Plot, plants, mean )# I want to sea the mean values, so I build df with mean values for each individual from our dataset
+a<-aggregate( . ~Plot, plants, mean )# To see the mean values, so I build df with mean values for each individual from our dataset
 
 gpsli <- interp(a$Longitude, a$Latitude, a$temp, duplicate = "median")#use interp function from akima to create interpolation for soil temperature
 
